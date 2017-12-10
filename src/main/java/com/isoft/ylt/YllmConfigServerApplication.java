e0b63af04ce210be5ad52e5eb1890bf690e91350
Edward <sun_yi@navercorp.com>
2017-12-10 16:06:05 +0800

    Initial commit

---
+++ src/main/java/com/isoft/ylt/YllmConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.isoft.ylt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YllmConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YllmConfigServerApplication.class, args);
+	}
+}
